006a5593d7576d074b561aee4603c666dee1178f
Luke Mulholland <lukemulholland@gmail.com>
2024-11-02 14:03:03 +1100

    Initial Commit

---
+++ TapZoo/TapZooApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapZooApp.swift
+//  TapZoo
+//
+//  Created by Luke Mulholland on 2/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TapZooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TapZooTests/TapZooTests.swift
@@ -0,0 +1,17 @@
+//
+//  TapZooTests.swift
+//  TapZooTests
+//
+//  Created by Luke Mulholland on 2/11/2024.
+//
+
+import Testing
+@testable import TapZoo
+
+struct TapZooTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
